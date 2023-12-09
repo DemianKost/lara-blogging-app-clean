673f3a9ec14391e72579256aa08b4e09f1e60689
DemianWHT <demiankost2@gmail.com>
2023-12-10 00:44:06 +0100

    Fixed ShowController - invoke action to show post by key

--- app/Http/Controllers/Api/V1/Posts/ShowController.php
+++ app/Http/Controllers/Api/V1/Posts/ShowController.php
@@ -1,15 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api\V1\Posts;
 
 use Domain\Blogging\Models\Post;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\PostResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ShowController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Post $post): JsonResponse
     {
-        
+        $post = QueryBuilder::for(
+            subject: $post
+        )->allowedIncludes(
+            includes: ['user']
+        )->first();
+
+        return response()->json(
+            data: new PostResource(
+                resource: $post
+            ),
+            status: 200
+        );
     }
 }
